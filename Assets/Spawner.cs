f64484d7842e232603de6a002707cebf59300700
Venus <inkyinked@gmail.com>
2018-10-04 12:59:08 +1000

    The spawner now instantiates cubes in a grid-like format.

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
+    public int MapSize;
+
+    private Vector3 SpawnLocation;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+		for(int i = 0; i <= MapSize; i++)
+        {
+            for (int j = 0; j <= MapSize; j++)
+            {
+                SpawnLocation.x = i;
+                Instantiate(ObjectToSpawn, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
 	}
 	
 	// Update is called once per frame
