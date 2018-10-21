1819ce0490127706e14a9e6ddfeb8685cab7dbf1
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-10-21 14:55:24 +1100

    Pushing the TouchControls I began working on, on Friday. Still trying to work out how to test them (unsure if they work).

---
+++ Levels/LevelDevelopment/Assets/Scripts/TouchControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour
+{
+	private Rigidbody2D playerCharacter;
+
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerCharacter = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+		{
+			// Get movement of finger since last frame
+			Vector2 touchPosition = Input.GetTouch(0).deltaPosition;
+				
+			// Move Character across X and Y planes
+			playerCharacter.transform.Translate(-touchPosition.x * speed, -touchPosition.y * speed, 0);
+		}
+
+		/*if (Input.GetMouseButtonDown(0))
+		{
+			// Get movement of finger since last frame
+			Vector2 touchPosition = Input.GetTouch(0).deltaPosition;
+
+			// Move Character across X and Y planes
+			playerCharacter.transform.Translate(-touchPosition.x * speed, -touchPosition.y * speed, 0);
+		}*/
+	}
+
+	void OnMouseDown ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Get movement of finger since last frame
+			Vector2 mousePosition = Input.mousePosition;
+
+			// Move Character across X and Y planes
+			playerCharacter.transform.Translate(-mousePosition.x * speed, -mousePosition.y * speed, 0);
+		}
+	}
+}
