2d6b2ad75dd5b9329c0a636de01f6bfd2ae6ea53
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-10-22 20:55:13 +1100

    Fixed issue with the TouchControl testing; Isn't behaving as intended, but it's a start (and can at least be tested!)

--- Levels/LevelDevelopment/Assets/Scripts/TouchControls.cs
+++ Levels/LevelDevelopment/Assets/Scripts/TouchControls.cs
@@ -6,7 +6,7 @@ public class TouchControls : MonoBehaviour
 {
 	private Rigidbody2D playerCharacter;
 
-	public float speed = 0.1f;
+	public float speed = 0.01f;
 
 	// Use this for initialization
 	void Start ()
@@ -17,37 +17,29 @@ public class TouchControls : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+		/*if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
 		{
 			// Get movement of finger since last frame
 			Vector2 touchPosition = Input.GetTouch(0).deltaPosition;
 				
 			// Move Character across X and Y planes
 			playerCharacter.transform.Translate(-touchPosition.x * speed, -touchPosition.y * speed, 0);
-		}
+		}*/
 
 		if (Input.GetMouseButtonDown(0))
 		{
-			/*Get movement of finger since last frame
-			Vector2 touchPosition = Input.GetTouch(0).deltaPosition;
-
-			Move Character across X and Y planes
-			playerCharacter.transform.Translate(-touchPosition.x * speed, -touchPosition.y * speed, 0);*/
 			OnMouseDown();
 		}
 	}
 
 	void OnMouseDown ()
 	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			// Get movement of finger since last frame
-			Vector2 mousePosition = Input.mousePosition;
+		// Get movement of finger since last frame
+		Vector2 mousePosition = Input.mousePosition;
 
-			// Move Character across X and Y planes
-			playerCharacter.transform.Translate(-mousePosition.x * speed, -mousePosition.y * speed, 0);
+		// Move Character across X and Y planes
+		playerCharacter.transform.Translate(-mousePosition.x * speed, -mousePosition.y * speed, 0);
 
-			Debug.Log("It works!");
-		}
+		Debug.Log("It works!");
 	}
 }
