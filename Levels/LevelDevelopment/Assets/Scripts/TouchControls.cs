ef8fc88279ab6ade1209b1bd80eaf2b8b84f8082
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-10-26 01:40:19 +1100

    Edited TouchControls script, now works smoother for mouse/testing/prototype purposes (still need to work out implementing and testing actual touch controls).

--- Levels/LevelDevelopment/Assets/Scripts/TouchControls.cs
+++ Levels/LevelDevelopment/Assets/Scripts/TouchControls.cs
@@ -4,48 +4,34 @@ using UnityEngine;
 
 public class TouchControls : MonoBehaviour
 {
-	private Rigidbody2D playerCharacter;
+	// private Rigidbody2D playerCharacter;
+
+	[SerializeField] private Transform target;
+
+	private Vector2 targetPos;
+
+	private float speed = 6f;
 
 	// public float speed = 0.01f;
 
 	// Use this for initialization
 	void Start ()
 	{
-		playerCharacter = GetComponent<Rigidbody2D>();
+		targetPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		/*if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-		{
-			// Get movement of finger since last frame
-			Vector2 touchPosition = Input.GetTouch(0).deltaPosition;
-				
-			// Move Character across X and Y planes
-			playerCharacter.transform.Translate(-touchPosition.x * speed, -touchPosition.y * speed, 0);
-		}*/
-
 		if (Input.GetMouseButtonDown(0))
 		{
-			OnMouseDown();
+			targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.position = targetPos;
 		}
-	}
 
-	void OnMouseDown ()
-	{
-		RaycastHit hit;
-
-		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+		if ((Vector2) transform.position != targetPos)
 		{
-			playerCharacter.transform.position = hit.point;
+			transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 		}
-		/*Get movement of finger since last frame
-		Vector2 mousePosition = Input.mousePosition;
-
-		// Move Character across X and Y planes
-		playerCharacter.transform.Translate(-mousePosition.x * speed, -mousePosition.y * speed, 0);
-
-		Debug.Log("It works!");*/
 	}
 }
