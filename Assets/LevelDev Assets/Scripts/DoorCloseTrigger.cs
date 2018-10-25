7b54ef7bf04f862340156ee790ae5d77309bca72
Luke <1009843@student.sae.edu.au>
2018-10-25 15:41:03 +1100

    Changed the grass layer order so it's behind everything

---
+++ Assets/LevelDev Assets/Scripts/DoorCloseTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCloseTrigger : MonoBehaviour
+{
+	public Door door;
+	public BasePlayerStats player;
+	public GameObject SelfTarget;
+
+	// Use this for initialization
+	void Start ()
+	{
+		door = FindObjectOfType<Door>();
+		door.enabled = false;
+		player = FindObjectOfType<BasePlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.GetComponent<BasePlayerStats>())
+		{
+			door.enabled = true;
+			Destroy(SelfTarget);
+			print("ASSASA");
+		}
+	}
+}
