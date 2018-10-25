b4fe8ef308f08985c03d51d621a3e8cee59919d9
Luke <1009843@student.sae.edu.au>
2018-10-25 13:03:13 +1100

    Scene change object is in

---
+++ Assets/LevelDev Assets/Scene Management/LevelEnd.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : SceneSwap
+{
+	public GameObject SelfTarget;
+	public BasePlayerStats player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = FindObjectOfType<BasePlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.GetComponent<BasePlayerStats>())
+		{
+			ChangeScene();
+			print("BEEP");
+		}
+	}
+
+}
