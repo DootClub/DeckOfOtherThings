90d2db3005cff0d3c89d970f1504070f1023e6b7
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-11-02 17:48:06 +1100

    Starting to work on card system; Card script created

---
+++ Assets/Programmer assets/Pscripts/Card.cs
@@ -0,0 +1,40 @@
+﻿	using System;
+	using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+	public event Action<Card> OnLevelEntered;
+	public event Action<Card> OnLevelExit; 
+
+	public int health;
+	public int damage;
+
+	public int moveSpeed;
+	public float fireRate;
+
+	public float visionRange;
+
+	public virtual void Start()
+	{
+		if (OnLevelEntered != null)
+		{
+			// Prompt to pick a card
+		}
+	}
+
+	public virtual void LevelExit()
+	{
+		if (OnLevelExit != null)
+		{
+			// Prompt to change card
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
