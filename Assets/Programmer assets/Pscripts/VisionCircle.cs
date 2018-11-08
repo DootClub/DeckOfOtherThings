bbda74588e8324ee2172244c61267e06361159db
Venus <inkyinked@gmail.com>
2018-11-08 12:59:40 +1100

    Created scene for player vision; created a TestPlayer prefab for future test scenes; created VisionCircle script and currently working on getting it to shoot towards whatever collided with it.

---
+++ Assets/Programmer assets/Pscripts/VisionCircle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionCircle : MonoBehaviour
+{
+    public float FireRate;
+    public GameObject Projectile;
+
+    private float LastFired;
+    public Vector3 DisplaceVector;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("Hit!");
+        if (Time.time - LastFired > 1 / (FireRate / 10))
+        {
+            LastFired = Time.time;
+            //Shoot();
+            Instantiate(Projectile, GetComponent<Transform>());
+        }
+        Instantiate(Projectile, collision.GetComponent<Transform>());
+    }
+}
