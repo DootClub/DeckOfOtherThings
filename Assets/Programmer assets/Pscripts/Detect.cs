46016cf2c5cc53822e88e8ac30b6d88ccf08dd81
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-11-13 13:27:26 +1100

    Created 'Target' prefab; Commented on Detect script

--- Assets/Programmer assets/Pscripts/Detect.cs
+++ Assets/Programmer assets/Pscripts/Detect.cs
@@ -2,23 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Written by Tegan Walsh-Jones
 public class Detect : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-
-	// Update is called once per frame
-	void Update ()
-    {
-	
-	}
-
+	// Runs when a trigger has been entered
 	void OnTriggerEnter2D (Collider2D other)
 	{
+		// If the collider 'other' has the tag "Enemy", announce that an enemy was detected
 		if (other.gameObject.tag == "Enemy")
 		{
 			Debug.Log("An enemy was detected");
