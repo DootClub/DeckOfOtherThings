e90a3ab61df190b40ec8aac7618557f3b677a73c
Venus <inkyinked@gmail.com>
2018-11-27 23:34:08 +1100

    The projectile's speed can now be either accelerating or constant.

--- Assets/Programmer assets/Pscripts/Projectile.cs
+++ Assets/Programmer assets/Pscripts/Projectile.cs
@@ -6,7 +6,7 @@ using System;
 public class Projectile : MonoBehaviour // Venus
 {
     // Setting up variables...
-    private Vector2 _speedVector;
+    //private Vector2 _speedVector;
 
     public float Speed;
     public float DespawnTime;
@@ -16,14 +16,16 @@ public class Projectile : MonoBehaviour // Venus
 	void Start ()
     {
         //FindObjectOfType<baseCard>().SomeEvent += SetSpeed;
-        _speedVector.x = Speed;
+        //_speedVector.x = Speed;
         Invoke("Despawn", DespawnTime);
 	}
 	
 	// Update is called once per frame
 	void Update ()
-    {
-        GetComponent<Rigidbody2D>().AddRelativeForce(_speedVector);
+	{
+	    //GetComponent<Rigidbody2D>().velocity = _speedVector;
+	    //GetComponent<Rigidbody2D>().AddRelativeForce(_speedVector);
+        transform.Translate((Vector3.right*Time.deltaTime*Speed));
 	}
 
     void OnTriggerEnter2D(Collider2D collision)
