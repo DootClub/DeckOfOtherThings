47a4369d9026aa71ea4bb5ca0f84d9a73ee17cae
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-11-28 17:08:05 +1100

    Created a pause menu prefab - including script; Created a scene to test the menu; Currently the pause button can't be clicked/isn't responding to click

---
+++ Assets/Programmer assets/Pscripts/PauseMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// Written by Tegan Walsh-Jones
+public class PauseMenu : MonoBehaviour
+{
+	public Canvas pauseMenu;
+
+	public static bool gameIsPaused = false;
+
+	[SerializeField] private Button pauseButton;
+
+	// Use this for initialization
+	void Start()
+	{
+		GetComponent<Canvas>();
+
+		pauseMenu.GetComponent<Canvas>().enabled = false;
+
+		pauseButton.onClick.AddListener(PauseOnClick);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void PauseOnClick ()
+	{
+		if (gameIsPaused)
+		{
+			Resume();
+		}
+
+		else
+		{
+			Pause();
+		}
+	}
+
+	public void Resume ()
+	{
+		pauseMenu.GetComponent<Canvas>().enabled = false;
+		Time.timeScale = 1f;
+		gameIsPaused = false;
+	}
+
+	public void Pause ()
+	{
+		pauseMenu.GetComponent<Canvas>().enabled = true;
+		Time.timeScale = 0f;
+		gameIsPaused = true;
+	}
+
+	public void LoadMainMenu ()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void QuitApp()
+	{
+		Application.Quit();
+	}
+}
