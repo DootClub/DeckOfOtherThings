ac23749fb87f6bb30281b5b7c1a4ee823412bb8a
Luke <1009843@student.sae.edu.au>
2018-11-29 17:00:17 +1100

    Dabbling in animation state changing

---
+++ Assets/LevelDev Assets/Scripts/AnimationState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationState : MonoBehaviour
+{
+
+	public Vector2 prevPos;
+	public Vector2 newPos;
+	public Transform player;
+	Animator animator;
+	float timer = 0.2F;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animator = GetComponent<Animator>();
+		player = GetComponent<Transform>();
+		newPos = player.localPosition;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timer -= Time.deltaTime;
+		prevPos = player.localPosition;
+		if(timer <= 0)
+		{
+			timer = 0.2F;
+			newPos = player.localPosition;
+			
+			if(newPos.x <= prevPos.x)
+			{
+				print("moving left");
+			}
+			else
+			{
+				print("moving right");
+			}
+		}
+
+
+	}
+}
