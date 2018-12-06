76bde5fcd9255b749910e1801c7726283f04411e
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-12-06 13:05:04 +1100

    Created Bulwark and Dreamer cards

---
+++ Assets/Programmer assets/Pscripts/Card System/Bulwark.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Written by Tegan Walsh-Jones
+public class Bulwark : CardBase
+{
+	// Use this for initialization
+	void Start ()
+	{
+		ChangeHealth();
+		ChangeMoveSpeed();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public override void ChangeHealth()
+	{
+		playerHealth = FindObjectOfType<Health>();
+
+		playerHealth.HealthAmount = 125;
+	}
+
+	public override void ChangeMoveSpeed()
+	{
+		playerMoveSpeed = FindObjectOfType<TouchControls>();
+
+		playerMoveSpeed.speed = 7.5f;
+	}
+}
---
+++ Assets/Programmer assets/Pscripts/Card System/Dreamer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Written by Tegan Walsh-Jones
+public class Dreamer : CardBase
+{
+	// Use this for initialization
+	void Start ()
+	{
+		ChangeHealth();
+		ChangeMoveSpeed();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public override void ChangeHealth()
+	{
+		playerHealth = FindObjectOfType<Health>();
+
+		if (Random.value < 0.5f)
+		{
+			playerHealth.HealthAmount = 150;
+		}
+
+		else
+		{
+			playerHealth.HealthAmount = 50;
+		}
+	}
+
+	public override void ChangeMoveSpeed()
+	{
+		playerMoveSpeed = FindObjectOfType<TouchControls>();
+
+		if (Random.value < 0.5f)
+		{
+			playerMoveSpeed.speed = 9;
+		}
+
+		else
+		{
+			playerMoveSpeed.speed = 3;
+		}
+	}
+}
