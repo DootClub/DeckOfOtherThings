efa9318e9e4cf27dfa308f086c4b8da1d72866fb
Luke <1009843@student.sae.edu.au>
2018-12-06 11:39:59 +1100

    Added a DetectPlayer script, when the player is in range of the enemy, the MoveTowarsPlayer script will activate

---
+++ Assets/LevelDev Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+	public Transform other;
+	float dist;
+	public float sightRange; 
+	public MoveTowardsPlayer self;
+
+	// Use this for initialization
+	void Start ()
+	{
+		self = GetComponent<MoveTowardsPlayer>();
+		self.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Distance();
+
+		if (dist <= sightRange)
+		{
+			self.enabled = true;
+			print("I'M COMIN FOR YA");
+		}
+		else
+		{
+			self.enabled = false;
+			print("o fuk where u go");
+		}
+	}
+
+	void Distance()
+	{
+		if (other)
+		{
+			dist = Vector2.Distance(other.position, transform.position);
+		}
+	}
+	
+}
