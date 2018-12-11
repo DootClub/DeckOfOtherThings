d3293953f305266202c1519b6239ec122d7e4c6b
Madison Delmenico <1010486@student.sae.edu.au>
2018-12-11 18:56:31 +1100

    Tweaks (health/buildsetting)
    health now clamped between 0-100, build settings now recognises 3rd level

--- Assets/Programmer assets/Pscripts/Health.cs
+++ Assets/Programmer assets/Pscripts/Health.cs
@@ -33,8 +33,8 @@ public class Health : MonoBehaviour // Venus
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+    }
 
     public int Change(int difference)
     {
